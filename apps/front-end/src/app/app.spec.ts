b4f2bcc5e74748076ebebaa662d8219edb91bdff
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-02-21 15:21:55 +0000

    Project set up

--- apps/front-end/src/app/app.spec.ts
+++ apps/front-end/src/app/app.spec.ts
@@ -5,7 +5,7 @@ import { NxWelcome } from './nx-welcome';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [ App, NxWelcome ]
     }).compileComponents();
   });
 
@@ -14,7 +14,7 @@ describe('App', () => {
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome front-end',
+      'Welcome front-end'
     );
   });
 });

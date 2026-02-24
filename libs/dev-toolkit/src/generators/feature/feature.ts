8cdd356a58d5d2f3329640f553400f52d387f984
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-02-24 16:43:31 +0000

    Added feature generator to dev toolkit

--- libs/dev-toolkit/src/generators/feature/feature.ts
+++ libs/dev-toolkit/src/generators/feature/feature.ts
@@ -1,5 +1,21 @@
-import { Tree } from '@nx/devkit';
+import { generateFiles, joinPathFragments, names, Tree } from '@nx/devkit';
+import { featurePath } from '../../lib/path-helper/path-helper';
 import { FeatureGeneratorSchema } from './feature-schema';
 
 export async function featureGenerator(tree: Tree, options: FeatureGeneratorSchema) {
+  const path = featurePath(tree, options.project, options.name);
+
+  if (tree.exists(path) && !tree.isFile(path)) {
+    console.log(`Feature "${options.name}" already exists.`);
+    return;
+  }
+
+  const { fileName } = names(options.name);
+
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, 'files'),
+    path,
+    { fileName }
+  );
 }

b767d0693141dc328d3ca0403dade0813b08511c
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-03-28 12:02:12 +0000

    Proper thumb loading with custom dimensions

--- apps/front-end/src/import/components/import-images/import-images.component.ts
+++ apps/front-end/src/import/components/import-images/import-images.component.ts
@@ -1,15 +1,18 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FileInfo, supportedFileExtensions } from '@lmgm/internal-api';
+import { ScaleManagerService } from '../../../device-scaling/services/scale-manager/scale-manager.service';
 import { FileSystemService } from '../../../file-system/services/file-system/file-system.service';
 import { TreeBranch } from '../../../ui/components/tree/tree.component.types';
 import { SourcesComponent } from '../sources/sources.component';
+import { thumbHeight, thumbWidth } from './import-images.component.types';
 
 @Component({
   selector: 'app-import-images',
   imports: [
     SourcesComponent,
-    JsonPipe
+    JsonPipe,
+    NgOptimizedImage
   ],
   templateUrl: './import-images.component.html',
   styleUrl: './import-images.component.scss',
@@ -18,11 +21,23 @@ import { SourcesComponent } from '../sources/sources.component';
 export class ImportImagesComponent {
   private readonly fileSystemService = inject(FileSystemService);
 
+  private readonly scaleManagerService = inject(ScaleManagerService);
+
+  protected readonly thumbWidth = thumbWidth;
+
+  protected readonly thumbHeight = thumbHeight;
+
+  protected readonly imageWidth = this.scaleManagerService.dimension(thumbWidth);
+
+  protected readonly imageHeight = this.scaleManagerService.dimension(thumbHeight);
+
   readonly files = signal<FileInfo[]>([]);
 
   readonly onDirChange = (branch: TreeBranch) => this.loadFiles(branch).then();
 
   private readonly loadFiles = async (branch: TreeBranch) => {
+    this.scaleManagerService.devicePixelRatio();
+
     this.files.set(
       (await this.fileSystemService.readDir(branch.id))
         .filter(item => !item.isDirectory)
